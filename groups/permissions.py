668bb602d34f0e93c54a9c0b95432f729e432d6d
Umesh Sharma <umesh@magicpin.in>
2018-04-01 01:43:45 +0530

    Enable group apis for superusers

--- groups/permissions.py
+++ groups/permissions.py
@@ -4,11 +4,14 @@ from rest_framework import permissions
 class IsGroupMember(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return obj.is_member(request.user)
+        return request.user.is_superuser or obj.is_member(request.user)
     
 class IsGroupAdminOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+
         if request.method in permissions.SAFE_METHODS:
             return True
 
@@ -17,16 +20,21 @@ class IsGroupAdminOrReadOnly(permissions.BasePermission):
 class IsGroupAdminOrMe(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+
     	return obj.group.is_admin(request.user) or obj.user == request.user
     
 class IsGroupAdmin(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-    	return obj.group.is_admin(request.user)
+    	return request.user.is_superuser or obj.group.is_admin(request.user)
     
 class CanAlterGroupMembership(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
         if obj.group.is_admin(request.user):
             return True
         if obj.user == request.user:
