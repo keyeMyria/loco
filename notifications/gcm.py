02e996cc0da350154d6031c3d08b50c91cc1a35f
Umesh Sharma <umesh@magicpin.in>
2018-07-05 12:21:11 +0530

    Verbose gcm failure

--- notifications/gcm.py
+++ notifications/gcm.py
@@ -34,7 +34,7 @@ def send_message_gcm(gcm_token, message_data):
 
 	res = requests.post(url, headers=headers, json=data)
 	if res.status_code >= 400:
-		raise Exception("GCMError")
+		raise Exception("GCMError", res.content)
 
 def send_notification_gcm(gcm_token, message_title, message_data='', message_body=""):
 	data= {
@@ -52,7 +52,7 @@ def send_notification_gcm(gcm_token, message_title, message_data='', message_bod
 		
 	res = requests.post(url, headers=headers, json=data)
 	if res.status_code >= 400 and res.status_code != 404:
-		raise Exception("GCMError")
+		raise Exception("GCMError", res.content)
 
 def send_checkin_gcm(checkin_id):
 	checkin = Checkin.objects.get(id=checkin_id)
