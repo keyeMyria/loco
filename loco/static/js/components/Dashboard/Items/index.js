b7f37fce66624d9432cfb99b5cdaf3b4c6990a12
Shubham <shubham.agrawal@magicpin.in>
2018-07-12 13:20:17 +0530

    redux fix

--- loco/static/js/components/Dashboard/Items/index.js
+++ loco/static/js/components/Dashboard/Items/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import { getItems } from '../reducer/dashboard';
+import { getItems } from '../../../reducer/dashboard';
 import DataList from './DataList'
 
 class Items extends Component {
--- loco/static/js/reducer/index.js
+++ loco/static/js/reducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
+import dashboard from './dashboard';
 
 const rootReducer = combineReducers({
   auth: auth,
+  dashboard: dashboard,
 });
 
 export default rootReducer;
