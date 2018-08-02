acb1a57c3db5050984cb5b2be6e68d99721a50f4
Shubham <shubham.agrawal@magicpin.in>
2018-08-02 16:11:23 +0530

    Download option changes

--- loco/static/js/components/Dashboard/Paginator.js
+++ loco/static/js/components/Dashboard/Paginator.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import Dialog from 'material-ui/Dialog';
 
 export default class Paginator extends Component {
 
     constructor(props) {
         super(props)
+        this.state = {
+            openDialog: false
+        }
     }
+
+    handleOpen = () => {
+        this.setState({openDialog: true});
+    };
+
+    handleClose = () => {
+        this.setState({openDialog: false});
+    };
+
 
     onNextClick = (event) => {
         event.stopPropagation();
@@ -47,17 +60,37 @@ export default class Paginator extends Component {
         if (this.props.csvURL) {
             downloadCSV = (
                 <section className="list-pager-action">
-                    <a target="_blank" className="pager-action" href={this.props.csvURL}>
-                        <i className="material-icons pager-action-icon">vertical_align_bottom</i> 
-                        <p className="pager-action-name">Download CSV</p>
-                    </a>
+                    <span target="_blank" className="pager-action" onClick={this.handleOpen}>
+                        <p className="pager-action-name">Download</p>
+                    </span>
                 </section>
             );
         }
 
+        var downloadDialog = (
+            <Dialog
+                title="Download As"
+                modal={false}
+                open={this.state.openDialog}
+                onRequestClose={this.handleClose} >
+
+                <a target="_blank" className="pager-action pager-downlaod-action" href={this.props.csvURL}>
+                    <i className="material-icons pager-action-icon">vertical_align_bottom</i> 
+                    <p className="pager-action-name">csv</p>
+                </a>
+
+                <a target="_blank" className="pager-action pager-downlaod-action" href={this.props.csvURL.replace("format=csv", "format=xlsx")}>
+                    <i className="material-icons pager-action-icon">vertical_align_bottom</i> 
+                    <p className="pager-action-name">xlsx</p>
+                </a>
+
+            </Dialog>
+        )
+
         return (
             <section className="list-pager">
                 {downloadCSV}
+                {downloadDialog}
                 <section className="list-pager-content">
                     <p className="pager-counter">{pageCounter}</p>
                     <i className={prevClass} onClick={this.onPrevClick}>keyboard_arrow_left</i>
