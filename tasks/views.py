a39014a8dbe4100e48409fbe8c12b2ab3ad0995e
Umesh Sharma <umesh@magicpin.in>
2018-08-24 11:45:43 +0530

    Fix imprts

--- tasks/views.py
+++ tasks/views.py
@@ -208,7 +208,7 @@ class TaskDetail(APIView):
         self.check_object_permissions(request, task)
         task.is_deleted = True
         task.save()
-        snapshot = TaskSnapshot.objects.get(task=task)
+        snapshot = models.TaskSnapshot.objects.get(task=task)
         snapshot.is_deleted = True
         snapshot.save()
         tasks.update_task_index_async.delay()
